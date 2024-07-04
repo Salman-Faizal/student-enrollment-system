ba2ca6120e4453c1bc42b9e1ecb2ccf9779e8651
Salman-Faizal <salmanfaizal1000@gmail.com>
2024-07-05 00:44:46 +0530

    Created 'students' app and defined 'Student' model:
    - Added the 'students' Django app including model with fields: name, email, current_status, created_at, and updated_at.
    - Configured and migrated the model to the database.

---
+++ students/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from funnel.models import FunnelStatus
+
+
+class Student(models.Model):
+  name = models.CharField(max_length=100)
+  email = models.EmailField(unique=True)
+  current_status = models.ForeignKey(FunnelStatus, on_delete=models.CASCADE)
+  created_at = models.DateTimeField(auto_now_add=True)
+  updated_at = models.DateTimeField(auto_now=True)
+
+  def __str__(self):
+    return f'{self.name} ({self.email} - Status: {self.current_status.name})'
