a64d51b95128cb3aa9868b589bb8e6deebe313dd
Salman-Faizal <salmanfaizal1000@gmail.com>
2024-07-05 03:38:05 +0530

    Further improvements to 'students' app: - Updated serializers.py to ensure proper serialization of Student model. - Configured API endpoint paths for the students app. - Defined StudentViewSet with default value setting for current_status(of student objects) upon creation. - Added custom constant in settings.py for DEFAULT_FUNNEL_STATUS. - Performed basic endpoint testing in DRF with no significant issues detected.

--- enrollment_funnel/settings.py
+++ enrollment_funnel/settings.py
@@ -124,3 +124,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Custom settings ----------
+
+# Default status name for new students
+DEFAULT_FUNNEL_STATUS = "Applied"
--- students/views.py
+++ students/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.conf import settings
+from django.core.exceptions import ValidationError
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Student
+from .serializers import StudentSerializer
+from funnel.models import FunnelStatus
 
-# Create your views here.
+
+class StudentViewSet(viewsets.ModelViewSet):
+  queryset = Student.objects.all()
+  serializer_class = StudentSerializer
+  permission_classes = [IsAuthenticated]
+  
+  def perform_create(self, serializer):
+    try:
+      default_status = FunnelStatus.objects.get(name=settings.DEFAULT_FUNNEL_STATUS)
+      serializer.save(current_status=default_status)
+    except FunnelStatus.DoesNotExist:
+      raise ValidationError(f"Default funnel status '{settings.DEFAULT_FUNNEL_STATUS}' does not exist.")
